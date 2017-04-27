b47c1c3f864a81ca81b1e47683ff9557c7fe617b
Davit Ohanyan <davitohanyan@gmail.com>
2017-04-27 16:10:24 +0200

    lets see what happens

--- src/index.js
+++ src/index.js
@@ -1,32 +1,44 @@
+"use strict";
+
 import * as YAML from 'yamljs';
 import toSource from 'tosource';
-import { createFilter, makeLegalIdentifier } from 'rollup-pluginutils';
+import {createFilter, makeLegalIdentifier} from 'rollup-pluginutils';
 
 const ext = /\.ya?ml$/;
+const parser = require('swagger-parser');
 
-export default function yaml ( options = {} ) {
-	const filter = createFilter( options.include, options.exclude );
+export default function yaml(options = {}) {
+  const filter = createFilter(options.include, options.exclude);
+  return {
+    name: 'yaml',
+    transform (yaml, id) {
+      return new Promise((resolve, reject) => {
+          parser.dereference(yaml)
+          .then((api) => {
+          if (!ext.test(id)) return resolve(null);
+      if (!filter(id)) return resolve(null);
 
-	return {
-		name: 'yaml',
+      var data = YAML.parse(api);
+      var keys = Object.keys(data).filter(function (key) {
+        return key === makeLegalIdentifier(key);
+      });
 
-		transform ( yaml, id ) {
-			if ( !ext.test( id ) ) return null;
-			if ( !filter( id ) ) return null;
+      var code = "var data = " + (toSource(data)) + ";\n\n";
 
-			const data = YAML.parse( yaml );
-			const keys = Object.keys( data ).filter( key => key === makeLegalIdentifier( key ) );
+      var exports = ['export default data;'].concat(
+        keys.map(function (key) {
+          return ("export var " + key + " = data." + key + ";");
+        })
+      ).join('\n');
 
-			let code = `var data = ${toSource( data )};\n\n`;
-
-			const exports = [ 'export default data;' ].concat(
-				keys.map( key => `export var ${key} = data.${key};` )
-			).join( '\n' );
-
-			return {
-				code: code + exports,
-				map: { mappings: '' }
-			};
-		}
-	};
+      return resolve({
+        code: code + exports,
+        map: {mappings: ''}
+      })
+    }).catch(() => {
+        resolve(null);
+    });
+    });
+    }
+  };
 }
